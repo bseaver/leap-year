0f70336ac8b047331b447584b4f8067da86f2691
Benjamin T. Seaver and David Rolfs <benjamin@>
2017-01-18 09:53:56 -0800

    add back end leap year function and implemented first rule.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-  alert("Document Ready");
-  
+
+  var leapYear = function(year) {
+    if (year === 1993) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  if (!leapYear(1997)) {
+    alert("1993 is not a leap year");
+  } else {
+    alert("1992 is a leap year");
+  }
+
+
 }); // End Document Ready
